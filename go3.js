5c99a34bdbe975bb2fd3a9aec3d3a257340a468d
ndquocan2024-ai <ndquocan2024@gmail.com>
2026-09-03 20:33:12 +0700

    Add files via upload

---
+++ go3.js
@@ -0,0 +1,13 @@
+let a = prompt("Nhập số a:");
+let b = prompt("Nhập số b:");
+
+a = Number(a);
+b = Number(b);
+
+if (b === 0) {
+    alert("Không thể chia cho 0!");
+} else if (a % b === 0) {
+    alert(a + " là bội số của " + b);
+} else {
+    alert(a + " không phải là bội số của " + b);
+}
